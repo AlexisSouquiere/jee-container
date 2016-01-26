fa1e2e45e74db6e7fee9c90ed2952850a30b8ded
Alexis Souquiere <alexis.souquiere@gmail.com>
2016-01-26 13:25:04 +0100

    Fix commit problem

--- 2-container/src/inject/spi/BootstrapInjector.java
+++ 2-container/src/inject/spi/BootstrapInjector.java
@@ -9,6 +9,9 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import org.reflections.Reflections;
+import org.reflections.scanners.FieldAnnotationsScanner;
+import org.reflections.util.ClasspathHelper;
+import org.reflections.util.ConfigurationBuilder;
 
 import inject.api.annotations.Inject;
 import inject.api.annotations.Prefered;
@@ -18,17 +21,28 @@ public class BootstrapInjector extends AbstractInjector {
 
 	private final static Logger LOGGER = Logger.getLogger(BootstrapedInjection.class.getName());
 
-	// TODO: faire le bind depuis le constructeur sur toutes les @Inject de
-	// toutes les classes du classpath
-	// Utilisation collector API Reflections
+	// Proxy avec Before/After / API Proxy de Java
 
-	@SuppressWarnings("unchecked")
-	public <T> void inject(Object instance) {
+	public <T> BootstrapInjector() {
+		Reflections reflections = new Reflections(new ConfigurationBuilder()
+				.setUrls(ClasspathHelper.forPackage("inject")).setScanners(new FieldAnnotationsScanner()));
+		// Get all the fields annotated with @Inject in the classpath
+		Set<Field> fieldsToInject = reflections.getFieldsAnnotatedWith(Inject.class);
+
+		for (Field fieldToInject : fieldsToInject) {
+			Class<?> klass = fieldToInject.getDeclaringClass();
+			if (map.get(klass) == null) {
+				injectDependencies(klass);
+			}
+		}
+	}
+
+	private <T> void injectDependencies(Class<?> klass) {
 		Reflections reflections = new Reflections("inject");
 
-		LOGGER.log(Level.INFO, "Getting all the fiels annotating with " + Inject.class.getName() + " in "
-				+ instance.getClass().getName());
-		Set<Field> fields = getAllFields(instance.getClass(), withAnnotation(Inject.class));
+		LOGGER.log(Level.INFO,
+				"Getting all the fiels annotating with " + Inject.class.getName() + " in " + klass.getName());
+		Set<Field> fields = getAllFields(klass, withAnnotation(Inject.class));
 
 		for (Field f : fields) {
 			Class<T> iClass = (Class<T>) f.getType(); // interface
@@ -48,39 +62,61 @@ public class BootstrapInjector extends AbstractInjector {
 				// Only one implementation
 				if (subTypes.size() == 1) {
 					subClass = (Class<? extends T>) subTypes.toArray()[0];
-				}
-				// More than 1 implementation : need to filter on @Prefered
-				else {
-					for (Class<? extends T> klass : subTypes) {
-						if (klass.isAnnotationPresent(Prefered.class)) {
-							subClass = klass;
-							break;
-						}
-					}
-
-					if (subClass == null) {
-						throw new RuntimeException("Several implementations were found. Use @Prefered to choose one");
-					}
+				} else {
+					// More than 1 implementation : need to filter on @Prefered
+					subClass = resolveMultipleImplementations(subTypes);
 				}
 
 				this.bind(iClass, subClass);
 			}
+		}
+	}
+
+	private <T> Class<? extends T> resolveMultipleImplementations(Set<Class<? extends T>> subTypes) {
+		Class<? extends T> subClass = null;
+
+		for (Class<? extends T> subType : subTypes) {
+			if (subType.isAnnotationPresent(Prefered.class)) {
+				subClass = subType;
+				break;
+			}
+		}
+
+		if (subClass == null) {
+			throw new RuntimeException("Several implementations were found. Use @Prefered to choose one");
+		}
+
+		return subClass;
+	}
+
+	@SuppressWarnings("unchecked")
+	public <T> void inject(Object instance) {
+		LOGGER.log(Level.INFO, "Getting all the fiels annotating with " + Inject.class.getName() + " in "
+				+ instance.getClass().getName());
+		Set<Field> fields = getAllFields(instance.getClass(), withAnnotation(Inject.class));
+
+		for (Field f : fields) {
+			Class<T> iClass = (Class<T>) f.getType(); // interface
 
 			try {
 				Object dependency = map.get(iClass);
 
 				// Inject dependencies recursively
 				this.inject(dependency);
-
-				// Process the injection
-				LOGGER.log(Level.INFO,
-						"Injecting " + dependency.getClass().getName() + " into " + instance.getClass().getName());
-				// Inject dependency into the instance
-				f.setAccessible(true);
-				f.set(instance, dependency);
+				this.processInjection(instance, f, dependency);
 			} catch (Exception e) {
 				throw new RuntimeException(e);
 			}
 		}
+	}
+
+	protected void processInjection(Object instance, Field f, Object dependency)
+			throws IllegalArgumentException, IllegalAccessException {
+		// Process the injection
+		LOGGER.log(Level.INFO,
+				"Injecting " + dependency.getClass().getName() + " into " + instance.getClass().getName());
+		// Inject dependency into the instance
+		f.setAccessible(true);
+		f.set(instance, dependency);
 	}
 }
